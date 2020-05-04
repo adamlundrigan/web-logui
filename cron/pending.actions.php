a0ce938d1008ee343b4a68d2ad1e1c688c3a67bc
Johan Harén <johan@halon.io>
2020-05-04 10:12:33 +0200

    update pending.actions.php

--- cron/pending.actions.php
+++ cron/pending.actions.php
@@ -20,73 +20,76 @@ try {
     $msgid = $row['msgid'];
     $actionid = $row['actionid'];
 
-    if ($msgid && $actionid) {
-      switch ($row['action']) {
-        case 'bounce':
-          echo "Bounce: $msgid:$actionid\n";
-          $client->operation('/protobuf', 'POST', null, [
-            'command' => 'G',
-            'program' => 'smtpd',
-            'payload' => [
-              'conditions' => [
-                'ids' => [
-                  [
-                    'transaction' => $msgid,
-                    'queue' => $actionid
+    try {
+      if ($msgid && $actionid) {
+        switch ($row['action']) {
+          case 'bounce':
+            echo "Bounce: $msgid:$actionid\n";
+            $client->operation('/protobuf', 'POST', null, [
+              'command' => 'G',
+              'program' => 'smtpd',
+              'payload' => [
+                'conditions' => [
+                  'ids' => [
+                    [
+                      'transaction' => $msgid,
+                      'queue' => $actionid
+                    ]
                   ]
-                ]
-              ],
-              'action' => 'BOUNCE'
-            ]
-          ]);
-          break;
-        case 'delete':
-          echo "Delete: $msgid:$actionid\n";
-          $client->operation('/protobuf', 'POST', null, [
-            'command' => 'G',
-            'program' => 'smtpd',
-            'payload' => [
-              'conditions' => [
-                'ids' => [
-                  [
-                    'transaction' => $msgid,
-                    'queue' => $actionid
-                  ]
-                ]
-              ],
-              'action' => 'DELETE'
-            ]
-          ]);
-          break;
-        case 'retry':
-          echo "Release: $msgid:$actionid\n";
-          $client->operation('/protobuf', 'POST', null, [
-            'command' => 'G',
-            'program' => 'smtpd',
-            'payload' => [
-              'conditions' => [
-                'ids' => [
-                  [
-                    'transaction' => $msgid,
-                    'queue' => $actionid
-                  ]
-                ]
-              ],
-              'move' => [
-                'queue' => 'ACTIVE'
+                ],
+                'action' => 'BOUNCE'
               ]
-            ]
-          ]);
-          break;
+            ]);
+            break;
+          case 'delete':
+            echo "Delete: $msgid:$actionid\n";
+            $client->operation('/protobuf', 'POST', null, [
+              'command' => 'G',
+              'program' => 'smtpd',
+              'payload' => [
+                'conditions' => [
+                  'ids' => [
+                    [
+                      'transaction' => $msgid,
+                      'queue' => $actionid
+                    ]
+                  ]
+                ],
+                'action' => 'DELETE'
+              ]
+            ]);
+            break;
+          case 'retry':
+            echo "Release: $msgid:$actionid\n";
+            $client->operation('/protobuf', 'POST', null, [
+              'command' => 'G',
+              'program' => 'smtpd',
+              'payload' => [
+                'conditions' => [
+                  'ids' => [
+                    [
+                      'transaction' => $msgid,
+                      'queue' => $actionid
+                    ]
+                  ]
+                ],
+                'move' => [
+                  'queue' => 'ACTIVE'
+                ]
+              ]
+            ]);
+            break;
+        }
+      } else {
+        echo "Skipping item ($msgid:$actionid)...\n";
       }
-    } else {
-      echo "Skipping item ($msgid:$actionid)...\n";
+    } catch (RestException $e) {
+      echo $row['msgid'].':'.$row['actionid'].' ('.$row['action'].'): '.$e->getMessage()."\n";
     }
+
     $cleanup = $dbh->prepare('DELETE FROM pending_actions WHERE msgid = :msgid AND actionid = :actionid;');
     $cleanup->execute([':msgid' => $row['msgid'], ':actionid' => $row['actionid']]);
   }
-} catch (RestException $e) {
-  echo $e->getMessage();
 } catch (PDOException $e) {
   echo $e->getMessage();
 }
