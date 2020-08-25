756700fdf968b5af1ca60994a6aad47ff9101fc9
Adam Lundrigan <adam@lundrigan.ca>
2020-08-25 15:33:47 -0230

    install.db.php: fix invalid "actionid" column definition

--- install.db.php
+++ install.db.php
@@ -16,7 +16,7 @@ $statement = $dbh->prepare('SELECT * FROM pending_actions LIMIT 1;');
 if (!$statement || $statement->execute() === false) {
   echo "Adding table `pending_actions`\r\n";
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $dbh->exec('CREATE TABLE pending_actions (msgid VARCHAR(100) NOT NULL, actionid, action VARCHAR(50) NOT NULL, serialno VARCHAR(50) NOT NULL, PRIMARY KEY(msgid, actionid));');
+  $dbh->exec('CREATE TABLE pending_actions (msgid VARCHAR(100) NOT NULL, actionid INT NOT NULL, action VARCHAR(50) NOT NULL, serialno VARCHAR(50) NOT NULL, PRIMARY KEY(msgid, actionid));');
 } else {
   echo "Table `pending_actions` already exists. Skipping...\r\n";
 }
